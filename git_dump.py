4e8126f065759eff37b6f8dc9f49c7ab75f22175
None <yamldeveloper@proton.me>
2024-05-08 07:25:33 +0300

    fixes

--- git_dump.py
+++ git_dump.py
@@ -3,6 +3,7 @@
 """\
 null-dependency git dumper
 """
+
 import argparse
 import codecs
 import functools
@@ -27,7 +28,7 @@ from urllib.error import HTTPError
 from urllib.parse import unquote, urljoin, urlsplit
 from urllib.request import HTTPHandler, HTTPSHandler, Request, build_opener
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Sergey M"
 
 GIT_DIR = "/.git/"
@@ -90,36 +91,43 @@ PACK_RE = re.compile(r"\bpack-" + SHA1_RE.pattern[2:-2] + r"\b")
 
 MAX_PARSE_SIZE = 1 << 27  # 128 MiB
 
-print = functools.partial(print, file=sys.stderr)
+print_stderr = functools.partial(print, file=sys.stderr)
 
 ASSET_EXTS = (
-    ".css",
-    ".html",
-    ".htm",
-    ".min.js",
-    ".css",
-    ".map",
-    ".jpeg",
-    ".jpg",
-    ".png",
-    ".gif",
+    ".ai",
+    ".apk",
     ".bmp",
-    ".ico",
-    ".eot",
-    ".ttf",
-    ".ttf2",
-    ".otf",
-    ".psd",
+    ".css",
+    ".css",
     ".doc",
     ".docx",
-    ".pdf",
+    ".eot",
+    ".gif",
+    ".htm",
+    ".html",
+    ".ico",
+    ".jpeg",
+    ".jpg",
+    ".less",
+    ".map",
+    ".min.js",
+    ".mo",
+    ".mov",
     ".mp3",
     ".mp4",
-    ".mov",
-    ".webm",
-    ".ai",
+    ".otf",
+    ".pdf",
+    ".png",
+    ".po",
+    ".psd",
+    ".scss",
+    ".svg",
     ".swf",
-    ".apk",
+    ".ttf",
+    ".ttf2",
+    ".webm",
+    ".woff",
+    ".woff2",
 )
 
 
@@ -162,9 +170,7 @@ def parse_args(argv):
         type=float,
         default=15.0,
     )
-    parser.add_argument(
-        "-A", "--user-agent", help="user-agent", default=USER_AGENT
-    )
+    parser.add_argument("-A", "--user-agent", help="user-agent", default=USER_AGENT)
     parser.add_argument(
         "-w",
         "--workers",
@@ -267,9 +273,9 @@ def humansize(n, base=1024, units="BKMGTPEZY"):
     if n == 0:
         return "0"
     exp = min(math.floor(math.log(n) / math.log(base)), len(units) - 1)
-    return "{:.2f} {}".format(
-        math.ceil(n / base**exp * 100) / 100, units[exp]
-    ).replace(".00", "")
+    return "{:.2f} {}".format(math.ceil(n / base**exp * 100) / 100, units[exp]).replace(
+        ".00", ""
+    )
 
 
 class DecodeError(Exception):
@@ -342,9 +348,7 @@ class GitDumper(Process):
         pos = decompressed.find(b"\x00")
         obj_type = decompressed[:pos].decode()
         if obj_type in [GIT_COMMIT_TYPE, GIT_TREE_TYPE]:
-            self.extract_hashes(
-                decompressed.decode(errors="ignore"), git_base_url
-            )
+            self.extract_hashes(decompressed.decode(errors="ignore"), git_base_url)
 
     def parse_git(self, temp_file, url):
         pos = url.find(GIT_DIR) + len(GIT_DIR)
@@ -353,10 +357,8 @@ class GitDumper(Process):
             for entry in GitIndex(temp_file).get_entries():
                 object_url = self.get_object_url(git_base_url, entry.sha1)
 
-                print(
-                    "\033[36mFound {} => {}\033[0m".format(
-                        object_url, entry.filename
-                    )
+                print_stderr(
+                    "\033[36mFound {} => {}\033[0m".format(object_url, entry.filename)
                 )
 
                 self.queue.put(object_url)
@@ -413,7 +415,7 @@ class GitDumper(Process):
                     break
                 # assert GIT_DIR in url
                 if url in self.seen:
-                    # print("\033[31mSeen: {}\033[0m".format(url))
+                    # print_stderr("\033[31mSeen: {}\033[0m".format(url))
                     continue
                 # файл может долго скачиваться, поэтому все же произойдет
                 # обработка одного и того же файла разными процессами
@@ -427,14 +429,14 @@ class GitDumper(Process):
                                 try:
                                     self.parse_git(temp_file, url)
                                 except DecodeError:
-                                    print(
+                                    print_stderr(
                                         "\033[31mNon zlib-deflated data: {}\033[0m".format(
                                             url
                                         )
                                     )
                                     continue
                             else:
-                                print(
+                                print_stderr(
                                     "\033[31mWARN: Too Large to Parse ({} > {}) - {}\033[0m".format(
                                         humansize(size),
                                         humansize(MAX_PARSE_SIZE),
@@ -446,9 +448,7 @@ class GitDumper(Process):
                         # hostname = sp.netloc.split(":")[0].strip("[]")
 
                         dest = (
-                            self.output_path
-                            / sp.netloc
-                            / unquote(sp.path.lstrip("/"))
+                            self.output_path / sp.netloc / unquote(sp.path.lstrip("/"))
                         )
 
                         try:
@@ -461,28 +461,24 @@ class GitDumper(Process):
                         # файл нельзя переместить куда-то в /home
                         # os.replace(temp_file.name, str(dest))
                         shutil.move(temp_file.name, str(dest))
-                        print("\033[32mSaved: {}\033[0m".format(dest))
+                        print_stderr("\033[32mSaved: {}\033[0m".format(dest))
                     except HTTPError as ex:
-                        print(
+                        print_stderr(
                             "\033[31mError {}: {}\033[0m".format(
                                 ex.status,
                                 url,
                             )
                         )
                     except socket.timeout:
-                        print("\033[31mTimeout: {}\033[0m".format(url))
+                        print_stderr("\033[31mTimeout: {}\033[0m".format(url))
                     finally:
                         if os.path.exists(temp_file.name):
                             os.unlink(temp_file.name)
             except Exception as ex:
-                print(
+                print_stderr(
                     "\033[31mAn Unexpected Error has occurred:",
                     "",
-                    "".join(
-                        traceback.format_exception(
-                            type(ex), ex, ex.__traceback__
-                        )
-                    ),
+                    "".join(traceback.format_exception(type(ex), ex, ex.__traceback__)),
                     "\033[0m",
                     sep=os.linesep,
                 )
@@ -498,10 +494,12 @@ def git_checkout_files(hosts, output_dir):
             if not repo_path.exists():
                 continue
             os.chdir(str(repo_path))
-            subprocess.check_call(["git", "checkout", "--", "."])
-            print("\033[32mRestored: {}\033[0m".format(repo_path))
+            # Исправил ошибку при которой удалялись выкаченные файлы
+            # shell=True полный аналон ['sh', '-c', '...']
+            subprocess.check_call("git checkout $(git ls-files -d)", shell=True)
+            print_stderr("\033[32mRestored: {}\033[0m".format(repo_path))
         except subprocess.CalledProcessError:
-            print("\033[31mCan't restore: {}\033[0m".format(repo_path))
+            print_stderr("\033[31mCan't restore: {}\033[0m".format(repo_path))
         finally:
             os.chdir(cwd)
 
@@ -544,12 +542,12 @@ def main(argv=None):
     for w in workers:
         w.join()
 
-    print("\033[33mDump tasks completed!\033[0m")
+    print_stderr("\033[33mDump tasks completed!\033[0m")
 
-    print("\033[33mGit checkout files\033[0m")
+    print_stderr("\033[33mGit checkout files\033[0m")
     hosts = set(urlsplit(u).netloc for u in seen.keys())
     git_checkout_files(hosts, args.output_dir)
-    print("\033[33mFinished!\033[0m")
+    print_stderr("\033[33mFinished!\033[0m")
 
 
 if __name__ == "__main__":
